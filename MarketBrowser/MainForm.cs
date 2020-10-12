4e44cfe68f4ab3144f78a1daa6522206bc9a4803
miracle3070 <miracle3070@naver.com>
2020-10-13 00:46:33 +0900

    Week 7-1 수업 내용

--- MarketBrowser/MainForm.cs
+++ MarketBrowser/MainForm.cs
@@ -18,7 +18,7 @@ namespace MarketBrowser
             InitializeComponent();
         }
 
-        private void buttonOpenCSV_Click(object sender, EventArgs e)
+        private List<List<string>> MakeCloumnarDataStructure()
         {
             StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open),
                 Encoding.Default);
@@ -27,24 +27,125 @@ namespace MarketBrowser
             var headers = line.Split(',');
             string str = "";
 
-            foreach(string header in headers)
+            List<List<string>> data = new List<List<string>>();
+            foreach (string header in headers)
             {
-                textBoxCSVViewer.Text += header + "\t";
+                List<string> list = new List<string>();
+                list.Add(header);
+                data.Add(list);
             }
 
-
-
-            /*
-            while(sr.EndOfStream == false)
+            int row = 0;
+            while (sr.EndOfStream == false)
             {
-                var line = sr.ReadLine();
-                str += line + "\r\n"; 
+                line = sr.ReadLine();
+                var values = line.Split(',');
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    data[i].Add(values[i]);
+                }
+
+                if (row++ == 10)
+                    break;
+            }
+
+            //textBoxCSVViewer.Text = str;
+
+            sr.Close();
+            return data;
+        }
+
+        private void printColumnData_columnbased(List<List<string>> data, int columnIndex)
+        {
+            string str = "";
+            textBoxCSVViewer.Text = "";
+            foreach (string value in data[1])
+            {
+                str += value + "\r\n";
+            }
+            textBoxCSVViewer.Text = str;
+
+        }
+
+        private void printRowData_columnbased(List<List<string>> data)
+        {
+            string str = "";
+            for (int row = 0; row < data[0].Count; row++)
+            {
+                foreach(List<string> list in data)
+                {
+                    str += list[row] + "\t";
+                }
+                str += "\r\n";
+            }
+            textBoxCSVViewer.Text = str;
+        }
+
+        // 과제: 이 함수를 적절하게 수정하여 과제방에 올릴것
+        private string[] parseCSVLine(string line)
+        {
+            return line.Split(',');
+        }
+
+        private List<List<string>> MakeRowbasedDataStructure()
+        {
+            StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open),
+                Encoding.Default);
+
+            var line = sr.ReadLine();
+
+            List<List<string>> data = new List<List<string>>();
+
+            int row = 0;
+            while (sr.EndOfStream == false)
+            {
+                line = sr.ReadLine();
+                var values = line.Split(',');
+
+                data.Add(values.ToList());
+
+                if (row++ == 10)
+                    break;
+            }
+
+            sr.Close();
+            return data;
+        }
+
+        private void printRowData_rowbased(List<List<string>> data)
+        {
+            string str = "";
+            foreach(List<string> rowlist in data)
+            {
+                foreach (string value in rowlist)
+                    str += value + "\t";
+                str += "\r\n";
             }
 
             textBoxCSVViewer.Text = str;
-            */
-            sr.Close();
+        }
 
+        private void printColumnData_rowbased(List<List<string>> data, int colIdx)
+        {
+            string str = "";
+            foreach (List<string> rowlist in data)
+            {
+                str += rowlist[colIdx] + "\r\n";
+            }
+            textBoxCSVViewer.Text = str;
+        }
+
+        private void buttonOpenCSV_Click(object sender, EventArgs e)
+        {
+            List<List<string>> data = MakeRowbasedDataStructure();
+            //printRowData_rowbased(data);
+            printColumnData_rowbased(data, 1);
+
+
+            //List<List<string>> data = MakeCloumnarDataStructure();
+            //printColumnData_columnbased(data, 1);
+            //printRowData_columnbased(data);
         }
     }
 }
